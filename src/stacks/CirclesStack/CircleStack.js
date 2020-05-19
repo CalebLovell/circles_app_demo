4c320a1dfd51f5921fdd8c1ee5191f5ecbf0a721
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-05-19 09:24:28 -0600

    add circle stack

---
+++ src/stacks/CirclesStack/CircleStack.js
@@ -0,0 +1,10 @@
+import React, { useContext} from 'react'
+import { View, Text } from 'react-native'
+
+export const CircleStack = () => {
+	return (
+		<View>
+			<Text>Circle Stack</Text>
+		</View>
+	)
+}
